a968ffb8cbed0912e4a05c7917487d1627c24ad7
Anna Kalinina <annakalinina13071993@gmail.com>
2021-07-29 14:56:13 +0300

    продолжила делать

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createMenuTemplate } from './view/menu.js';
 import { createSortTemplate } from './view/sort.js';
 import { createFilmTemplate } from './view/films.js';
 import { createFilmsContainer } from './view/filmcontainer.js';
+import { createButtonTemplate } from './view/button.js';
+import { createTopContainer } from './view/topContainer.js';
+import { createTopFilmsTemplate } from './view/topFilms.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -11,15 +14,16 @@ const render = (container, template, place) => {
 const siteHeaderElement = document.querySelector('.header');
 const siteLogoHeaderElement = siteHeaderElement.querySelector('.header__logo');
 const siteMainElement = document.querySelector('.main');
-
 render(siteLogoHeaderElement, createLogoTemplate(), 'beforeend');
 render(siteMainElement, createMenuTemplate(), 'beforeend');
 render(siteMainElement, createSortTemplate(), 'beforeend');
 render(siteMainElement, createFilmsContainer(), 'beforeend');
-
-const siteSectionElement = siteMainElement.querySelector('.films-list');
-const siteDivContainerElement = siteSectionElement.querySelector('.films-list__container');
+const siteSectionElement = siteMainElement.querySelector('.films');
+const siteSectionFilmsElement = siteSectionElement.querySelector('.films-list');
+const siteDivContainerElement = siteSectionFilmsElement.querySelector('.films-list__container');
 for (let i = 1; i <= 5; i++) {
   render(siteDivContainerElement, createFilmTemplate(), 'beforeend');
 }
+render(siteSectionFilmsElement, createButtonTemplate(), 'beforeend');
+render(siteSectionElement, createTopContainer(), 'beforeend');
 
