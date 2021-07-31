944670422afed850377ecba2829b6b3d3b9578f7
Anna Kalinina <annakalinina13071993@gmail.com>
2021-07-31 12:27:43 +0300

    исправила замечания по первому модулю

--- src/main.js
+++ src/main.js
@@ -1,47 +1,52 @@
+import { render } from './utils/render.js';
 import { createLogoTemplate } from './view/logo.js';
 import { createMenuTemplate } from './view/menu.js';
 import { createSortTemplate } from './view/sort.js';
 import { createFilmTemplate } from './view/films.js';
 import { createFilmsContainer } from './view/filmcontainer.js';
 import { createButtonTemplate } from './view/button.js';
-import { createTopContainer } from './view/topContainer.js';
+import { createAdditionalContainer } from './view/additional-containers.js';
 import { createTopFilmTemplate } from './view/topFilms.js';
-import { createMostCommentedContainer } from './view/mostCommentedContainer.js';
 import { createMostCommentedFilmsTemplate } from './view/mostCommentedFilms.js';
 import { createNumbersFilms } from './view/numbersFilms.js';
 import { createPopupTemplate } from './view/popup.js';
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+const INSERT_PLACE = {
+  beforeend: 'beforeend',
+  afterend: 'afterend',
+  beforebegin: 'beforebegin',
+  afterbegin: 'afterbegin',
 };
+const TOP_NAME = 'Top rated';
+const MOST_COMMENTED_NAME = 'Most commented';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
-render(siteHeaderElement, createLogoTemplate(), 'beforeend');
-render(siteMainElement, createMenuTemplate(), 'beforeend');
-render(siteMainElement, createSortTemplate(), 'beforeend');
-render(siteMainElement, createFilmsContainer(), 'beforeend');
+render(siteHeaderElement, createLogoTemplate(), INSERT_PLACE.beforeend);
+render(siteMainElement, createMenuTemplate(), INSERT_PLACE.beforeend);
+render(siteMainElement, createSortTemplate(), INSERT_PLACE.beforeend);
+render(siteMainElement, createFilmsContainer(), INSERT_PLACE.beforeend);
 const siteSectionElement = siteMainElement.querySelector('.films');
 const siteSectionFilmsElement = siteSectionElement.querySelector('.films-list');
 const siteDivContainerElement = siteSectionFilmsElement.querySelector('.films-list__container');
 for (let i = 1; i <= 5; i++) {
-  render(siteDivContainerElement, createFilmTemplate(), 'beforeend');
+  render(siteDivContainerElement, createFilmTemplate(), INSERT_PLACE.beforeend);
 }
-render(siteSectionFilmsElement, createButtonTemplate(), 'beforeend');
-render(siteSectionElement, createTopContainer(), 'beforeend');
+render(siteSectionFilmsElement, createButtonTemplate(), INSERT_PLACE.beforeend);
+render(siteSectionElement, createAdditionalContainer(TOP_NAME), INSERT_PLACE.beforeend);
 const siteSectionTopFilmsElement = siteSectionElement.querySelector('.films-list--extra');
 const siteTopDivContainerElement = siteSectionTopFilmsElement.querySelector('.films-list__container');
 for (let i = 1; i <= 2; i++) {
-  render(siteTopDivContainerElement, createTopFilmTemplate(), 'beforeend');
+  render(siteTopDivContainerElement, createTopFilmTemplate(), INSERT_PLACE.beforeend);
 }
-render(siteSectionElement, createMostCommentedContainer(), 'beforeend');
+render(siteSectionElement, createAdditionalContainer(MOST_COMMENTED_NAME), INSERT_PLACE.beforeend);
 const siteSectionFilmsElements = siteSectionElement.querySelectorAll('.films-list--extra');
 const siteMostCommentedDivContainerElement = siteSectionFilmsElements[1].querySelector('.films-list__container');
 for (let i = 1; i <= 2; i++) {
-  render(siteMostCommentedDivContainerElement, createMostCommentedFilmsTemplate(), 'beforeend');
+  render(siteMostCommentedDivContainerElement, createMostCommentedFilmsTemplate(), INSERT_PLACE.beforeend);
 }
 const siteFooterElement = document.querySelector('.footer');
 const siteFooterSectionElement = siteFooterElement.querySelector('.footer__statistics');
-render(siteFooterSectionElement, createNumbersFilms(), 'beforeend');
-const siteBodyElement = document.querySelector ('body');
-render(siteBodyElement, createPopupTemplate(), 'beforeend');
+render(siteFooterSectionElement, createNumbersFilms(), INSERT_PLACE.beforeend);
+const siteBodyElement = document.querySelector('body');
+render(siteBodyElement, createPopupTemplate(), INSERT_PLACE.beforeend);
--- src/view/mostCommentedContainer.js
+++
@@ -1,8 +0,0 @@
-export const createMostCommentedContainer = () => (
-  `<section class="films-list films-list--extra">
-  <h2 class="films-list__title">Most commented</h2>
-
-  <div class="films-list__container">
-  </div>
-  </section>`
-);
